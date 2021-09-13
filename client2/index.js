7ddebd1199e0cda4bc6880ee3d97e08ccbfa79ea
Andrei Gaspar <andrei@planeeet.com>
2021-09-13 11:10:28 +0200

    New proto integration

--- client2/index.js
+++ client2/index.js
@@ -2,18 +2,30 @@ const PROTO_PATH = __dirname + '/../proto/random.proto';
 
 const grpc = require('@grpc/grpc-js');
 const loader = require('@grpc/proto-loader');
+const fs = require('fs');
+const path = require('path');
 
 const packageDef = loader.loadSync(PROTO_PATH);
 
 const randomProto = grpc.loadPackageDefinition(packageDef).randomPackage;
 
 const address = '3.68.194.144:8082';
-// const address = 'localhost:8082';
+// const address = 'ec2-3-68-194-144.eu-central-1.compute.amazonaws.com:8082';
+// const address = '0.0.0.0:8082';
 
-const client = new randomProto.Random(
-  address,
-  grpc.credentials.createInsecure()
+const metadataCreds = grpc.credentials.createFromMetadataGenerator((_, cb) => {
+  const metadata = new grpc.Metadata();
+  metadata.set('Authorization', 'testtoken12312312');
+  cb(null, metadata);
+});
+
+const rootCert = fs.readFileSync(
+  path.resolve(__dirname, '../certs/ca-cert.pem')
 );
+
+const sslCreds = grpc.credentials.createSsl(rootCert);
+
+const client = new randomProto.Random(address, sslCreds);
 
 const deadline = new Date();
 deadline.setSeconds(deadline.getSeconds() + 5);
